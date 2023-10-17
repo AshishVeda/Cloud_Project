1f4a76d4cca986a306f81397eb5bb84b1d937d4a
AshishVeda <vedaashish2@gmail.com>
2023-10-17 04:16:07 -0700

    Changed form action

--- app.js
+++ app.js
@@ -125,10 +125,10 @@ app.post('/auth/login', (req, res) => {
                     
                     const token = jwt.sign({ username: user.UserName, id: user.UserId }, JWT_SECRET, { expiresIn: '1h' });
 
-                    res.cookie('token', token, {SameSite:'None', secure: true });
+                    res.cookie('token', token, {SameSite:'Strict' });
                     const userRole = user.UserRole;
 
-                    res.cookie('role', userRole, {SameSite:'None', secure: true });
+                    res.cookie('role', userRole, {SameSite:'Strict'});
                     if (userRole === 'admin') {
                         // res.redirect("/admin/dashboard"); 
                         res.redirect('http://44.192.39.79:4000/admin/dashboard');
